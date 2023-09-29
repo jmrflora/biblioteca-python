38a9acdfecd53a692be09d1958f275bd0b64f9b4
José Matheus R. Flora <jmrfloraxbox@gmail.com>
2023-09-29 14:46:50 -0300

    basic shit working

--- projetoBiblioV2/backend/app/main.py
+++ projetoBiblioV2/backend/app/main.py
@@ -1,15 +1,20 @@
+import sqlmodel
 import uvicorn
 from fastapi import FastAPI
-
+from sqlmodel import SQLModel
 
 from backend.app import settings
+from backend.app.schemas import models
 from backend.app.schemas.models import HealthCheck
-
+from backend.app.core.db import engine
 
 app = FastAPI(
     title=settings.project_name,
     version=settings.version
 )
+
+
+SQLModel.metadata.create_all(engine)
 
 
 @app.get('/', response_model=HealthCheck, tags=["status"])
