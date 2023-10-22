2f6b8cf48020481d3ccf1bc1b7312f433e84c52d
José Matheus R. Flora <jmrfloraxbox@gmail.com>
2023-10-22 20:12:39 -0300

    devolucoes devidamente protegidas eu acho

--- projetoBiblioV3/biblioteca/backend/app/api/devolucao.py
+++ projetoBiblioV3/biblioteca/backend/app/api/devolucao.py
@@ -1,23 +1,55 @@
 from typing import Annotated, List
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session, select
+from backend.app.api.auth import get_current_user
 
 from backend.app.core.db import get_session
-from backend.app.schemas.models import Devolucao, DevolucaoRead, DevolucaoCreate, DevolucaoReadComEmprestimo
+from backend.app.schemas.models import Devolucao, DevolucaoRead, DevolucaoCreate, DevolucaoReadComEmprestimo, Exemplar, Usuario
 
 router = APIRouter()
 
 db_dependency = Annotated[Session, Depends(get_session)]
-
+user_dependecy = Annotated[dict, Depends(get_current_user)]
 
 @router.get('/', response_model=List[DevolucaoRead])
-def get_all_devolucao(db: db_dependency):
-    db_devolucoes = db.exec(select(Devolucao)).all()
+def get_all_devolucao(db: db_dependency, admin: user_dependecy, cliente_id: Annotated[int | None, Query()] = None, exemplar_id: Annotated[int | None, Query()] = None):
+    db_admin = db.get(Usuario, admin.get('id'))
+    
+    if not db_admin:
+        raise HTTPException(status_code=404, detail="admin nao encontrado")
+    if db_admin.tipo.value != "admin":
+        raise HTTPException(status_code=401, detail="not an admin")
+    
+    statement = select(Devolucao)
+    
+    if cliente_id != None:
+        db_cliente = db.get(Usuario, cliente_id)
+        if not db_cliente:
+            raise HTTPException(status_code=404, detail="cliente not found")
+        
+        statement = statement.where(Devolucao.emprestimo.has(usuario=db_cliente))
+        # statement = statement.where(Devolucao.emprestimo.usuario == db_cliente)
+        
+    if exemplar_id != None:
+        db_exemplar= db.get(Exemplar, exemplar_id)
+        if not db_exemplar:
+            raise HTTPException(status_code=404, detail="exemplar not found")
+        statement = statement.where(Devolucao.emprestimo.has(exemplar=db_exemplar))
+        
+        
+    db_devolucoes = db.exec(statement).all()
     return db_devolucoes
 
 
 @router.get('/{devolucao_id}', response_model=DevolucaoReadComEmprestimo)
-def get_devolucao(db: db_dependency, devolucao_id: int):
+def get_devolucao(db: db_dependency, devolucao_id: int, admin: user_dependecy):
+    db_admin = db.get(Usuario, admin.get('id'))
+    
+    if not db_admin:
+        raise HTTPException(status_code=404, detail="admin nao encontrado")
+    if db_admin.tipo.value != "admin":
+        raise HTTPException(status_code=401, detail="not an admin")
+    
     db_devolucao = db.get(Devolucao, devolucao_id)
     if not db_devolucao:
         raise HTTPException(status_code=404, detail="devolucao not found")
@@ -26,7 +58,14 @@ def get_devolucao(db: db_dependency, devolucao_id: int):
 
 
 @router.post('/', response_model=DevolucaoRead)
-def create_devolucao(db: db_dependency, devolucao_form: DevolucaoCreate):
+def create_devolucao(db: db_dependency, devolucao_form: DevolucaoCreate, admin: user_dependecy):
+    db_admin = db.get(Usuario, admin.get('id'))
+    
+    if not db_admin:
+        raise HTTPException(status_code=404, detail="admin nao encontrado")
+    if db_admin.tipo.value != "admin":
+        raise HTTPException(status_code=401, detail="not an admin")
+    
     db_devolucao = Devolucao.from_orm(devolucao_form)
     db.add(db_devolucao)
     db.commit()
@@ -35,7 +74,14 @@ def create_devolucao(db: db_dependency, devolucao_form: DevolucaoCreate):
 
 
 @router.delete('/{devolucao_id}')
-def delete_devolucao(db: db_dependency, devolucao_id: int):
+def delete_devolucao(db: db_dependency, devolucao_id: int, admin: user_dependecy):
+    db_admin = db.get(Usuario, admin.get('id'))
+    
+    if not db_admin:
+        raise HTTPException(status_code=404, detail="admin nao encontrado")
+    if db_admin.tipo.value != "admin":
+        raise HTTPException(status_code=401, detail="not an admin")
+    
     db_devolucao = db.get(Devolucao, devolucao_id)
     if not db_devolucao:
         raise HTTPException(status_code=404, detail="devolucao not found")
