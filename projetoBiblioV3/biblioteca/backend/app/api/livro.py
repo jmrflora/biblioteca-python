98ee72546966e8c6b9bae8e240f9b4eb11ee519b
jose matheus <joseribeiroflora@gmail.com>
2023-11-30 16:05:51 -0300

    novo endpoint de disponibilidade

--- projetoBiblioV3/biblioteca/backend/app/api/livro.py
+++ projetoBiblioV3/biblioteca/backend/app/api/livro.py
@@ -1,17 +1,21 @@
 from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlmodel import Session, select
+from sqlmodel import Session, select, SQLModel
 from backend.app.api.auth import get_current_user
 
 from backend.app.core.db import get_session
 from backend.app.schemas.models import Livro, LivroRead, LivroCreate, LivroUpdate, LivroComExemplares, Exemplar, \
     ExemplarRead, \
-    ExemplarCreate, ExemplarReadComLivro, Usuario
+    ExemplarCreate, ExemplarReadComLivro, Usuario, Devolucao
 
 router = APIRouter()
 
 db_dependency = Annotated[Session, Depends(get_session)]
 user_dependecy = Annotated[dict, Depends(get_current_user)]
+
+class disponibilidade(SQLModel):
+    disponivel: bool
+    msg: str
 
 @router.get('/', response_model=List[LivroRead])
 def get_all_livros(db: db_dependency, autor: Annotated[str | None, Query()] = None):
@@ -32,6 +36,38 @@ def get_livro_by_id(db: db_dependency, livro_id: int):
         raise HTTPException(status_code=404, detail="livro nao encontrado")
 
     return db_livro
+
+@router.get('/disponibilidade/{livro_id}', response_model=disponibilidade)
+def check_disponibilidade(db: db_dependency,usuario: user_dependecy,livro_id: int, tipo: Annotated[str | None, Query()] = None):
+    db_usuario = db.get(Usuario, usuario.get('id'))
+    
+    if not db_usuario:
+        raise HTTPException(status_code=404, detail="Usuario nao encontrado")
+    
+    db_livro = db.get(Livro, livro_id)
+
+    if not db_livro:
+        raise HTTPException(status_code=404, detail="livro not found")
+    
+    db_exemplares = db_livro.exemplares
+    if not db_exemplares:
+        return {"disponivel": False, "msg": "sem exemplares"}
+    # pegar cada exemplar do livro
+    for exemplar in db_exemplares:
+        # pegar o ultimo emprestimo desse exemplar
+        try:
+            db_emprestimo = exemplar.usuario_links[-1]
+        except IndexError:
+            return{"disponivel": True, "msg": "sem emprestimos"}
+
+        # ver se esse emprestimo possui devolução
+        db_devolucao = db.exec(select(Devolucao).where(Devolucao.emprestimo == db_emprestimo)).first()
+        # se tiver, retornar disponível
+        if db_devolucao:
+            return {"disponivel": True, "msg": "tudo ok"}
+
+    return {"disponivel": False, "msg": "erro"}
+
 
 
 @router.patch('/{livro_id}', response_model=LivroComExemplares)
